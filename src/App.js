094be9f47a172f40babe10a8c4dce43662988264
aliibnemasud <aliibnemasud@gmail.com>
2022-05-04 16:54:54 +0600

    Banner updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Banner from './componants/Banner/Banner';
 import Footer from './componants/Footer/Footer';
 import Header from './componants/Header/Header';
 import Home from './componants/Home/Home';
@@ -8,9 +9,11 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
       <Routes>
         <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
+
       <Footer></Footer>
     </div>
   );
---
+++ src/componants/Banner/Banner.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import c1 from '../img/c1.jpg';
+import c2 from '../img/c2.jpg';
+import c3 from '../img/c3.jpg';
+
+const Banner = () => {
+    return (
+        <Carousel variant="dark">
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={c1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h5 className='text-light'>First slide label</h5>
+                    <p className='text-light'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={c2}
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h5 className='text-light'>Second slide label</h5>
+                    <p className='text-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={c3}
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h5 className='text-light'>Third slide label</h5>
+                    <p className='text-light'>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Banner;
