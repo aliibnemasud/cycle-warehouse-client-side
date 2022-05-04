7a1f1d153dc20059c61f2006f23d85b8ae367c1c
aliibnemasud <aliibnemasud@gmail.com>
2022-05-04 06:05:38 +0600

    Client site structure complete

---
+++ src/componants/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <img src="" alt="" />
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/store">Store</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
