895b44c8175708a5a7d113f1dc0f08efc418de1e
aliibnemasud <aliibnemasud@gmail.com>
2022-05-08 23:09:16 +0600

    Home Link added

--- src/componants/Error/Error.js
+++ src/componants/Error/Error.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div className="text-center my-5">
             <h1 className="fw-bold">404</h1>
-            <p>Page Not Found!</p>
+            <p>Page Not Found! <Link to='/home'>Home</Link></p>
         </div>
     );
 };
